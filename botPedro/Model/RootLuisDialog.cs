cfe3f597403cd4bcd8029dfea0aa6375ac16fd05
Pedro Henrique Arthur <t-peart@microsoft.com>
2016-11-07 14:21:09 -0200

    subindo os arquivos...

---
+++ botPedro/Model/RootLuisDialog.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace botPedro.Model
+{
+    [LuisModel("e3866536-b821-462f-b2ce-f6913f97e8dc", "d62c7688831a4ab0871f3e3cb0e71e15")]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        [LuisIntent("None")]
+        public async Task NoneAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Desculpe, eu não entendi...");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Saudacao")]
+        public async Task OiAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Olá, tudo bem?");
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
---
+++ botPedro/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="botPedro.WebApiApplication" Language="C#" %>
